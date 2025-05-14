3a3a3851990b7e02a0cfcf63377e8f67d98d53a5
Saswater <saswata_majumder@brown.edu>
2025-05-14 18:41:06 -0400

    Added validation, included examples for different accesses including errors, and refactored a bit

--- test/lmap.c
+++ test/lmap.c
@@ -2,7 +2,6 @@
 #define LMAP_C
 
 #include "main.h"
-#include "llist.c"
 
 // Types
 // all the metadata
@@ -22,7 +21,7 @@ typedef struct lmap_node_s {
 
 // uint64_t* lock_persist;
 // GLOBALS
-uint64_t key_cnt = 0;
+uint64_t key_cnt = INVALID_KEY + 1;
 // always points to the start of the list
 // lmap_node* lmap_start = (lmap_node*)NULL;
 // lmap_node lmap_start = {{0}, (uintptr_t)NULL, (lmap_node*)NULL};
@@ -37,6 +36,19 @@ void lmap_print(lmap_node*);
 uint64_t new_key() {
     key_cnt++;
     return key_cnt;
+}
+
+
+
+
+
+// Creates a new lock, initialises it with the key, and returns its address.
+uint64_t* new_lock(uint64_t key) {
+    // alloc a new lock lock_addr
+    uint64_t* lock_addr = MALLOC_T(uint64_t);                                            /*MALLOC*/
+    CHK_PTR(lock_addr);
+    *lock_addr = key;
+    return lock_addr;
 }
 
 
@@ -61,31 +73,63 @@ void lmap_print(lmap_node* start) {
 }
 
 
+lmap_node* lmap_find(lmap_node* start, uintptr_t addr) {
+    lmap_node* curr_node = start;
+
+    while (curr_node) {
+        if (curr_node->addr == addr)
+            return curr_node;
+        curr_node = curr_node->next;
+    }
+
+    return NULL;
+}
+
+
 lmap_node* lmap_add(
     lmap_node** lmap_start,
-    addr_node** llist_start,
     uintptr_t addr,
     uintptr_t base,
     uintptr_t bound,
     PtrFreeable freeable
 ) {
     if (!addr) {
-        DBG("Will not map a NULL ptr - returning!");
+        DBG("Will not map a NULL ptr!");
         return NULL;
     }
-
-    // ptr_meta* mtdt = (ptr_meta*)malloc(sizeof(ptr_meta));
+    // avoid duplicates, since the same addr should have the same key
+    //      actually, should be okay! we can just copy over the details
+    // if (lmap_find(*lmap_start, addr)) {
+    //     DBG("Address %p already exists in lmap - not adding again!\n", (void*)addr);
+    //     return NULL;
+    // }
+    
     ptr_meta* mtdt = MALLOC_T(ptr_meta);                                                /*MALLOC*/
     CHK_PTR((void*)mtdt);
     lmap_node* new_node = MALLOC_T(lmap_node);                                          /*MALLOC*/
     CHK_PTR((void*)new_node);
+    
+    // the following is based on the assumption that if two addrs (key) are the same in the lmap,
+    //      then their metadata->base_addrs will also be the same (vice versa not necessarily true)
+    //      could add checks for this?
+    // lmap_node* base_node = lmap_find(*lmap_start, addr);
+    // if base =/= addr (due to ptr arithmetic), then base could also already exist
+    //      if so, propagate metadata!
+    lmap_node* base_node = lmap_find(*lmap_start, base);
+    // if (!base_node)
+    //     base_node = lmap_find(*lmap_start, base);
+    if (base_node) {
+        DBG("Base %p already exists in lmap - copying metadata!\n", (void*)addr);
+        *mtdt = *(base_node->metadata);
+    } else {
+        mtdt->base_addr  = base;
+        mtdt->bound_addr = bound;
+        // generate new key and lockaddr pair ONLY if neither the ptr nor the baseaddr already exist in lmap
+        mtdt->key       = new_key();
+        mtdt->lock_addr = new_lock(mtdt->key);
+    }
 
-    mtdt->base_addr = base;
-    mtdt->bound_addr = bound;
     mtdt->freeable = freeable;
-    mtdt->key = new_key();
-    // this changes the global llist_start
-    mtdt->lock_addr = add_addr(llist_start, mtdt->key)->lock_addr;
     new_node->addr = addr;
     new_node->metadata = mtdt;
 
@@ -97,20 +141,25 @@ lmap_node* lmap_add(
 }
 
 
-// Deletes all nodes with matching addrs, including their LOCKED ADDRESSes' entries in the llist.
+// Deletes the first/all node(s) with matching addrs, along with all associated information like the lock.
+//  Frees the ptr, if FREEABLE.
 void lmap_del(
     lmap_node** lmap_start,
-    addr_node** llist_start,
     uintptr_t addr
 ) {
     lmap_node* curr_node = *lmap_start;
     lmap_node* prev_node = (lmap_node*)NULL;
     uint_fast8_t found = 0;
+    uint_fast8_t ptr_freed = 0;
 
     while (curr_node) {
         if (curr_node->addr == addr) {
             // match found!
             found = 1;
+            if ((curr_node->metadata->freeable == FREEABLE) && (!ptr_freed)) {
+                free((void*)addr);
+                ptr_freed = 1;
+            }
             // need to fill in gap!
             //      if start (no prev node), NEED TO CHANGE GLOBAL START
             //      if mid or end, then change prev_node->next to curr_node->next
@@ -120,7 +169,10 @@ void lmap_del(
                 *lmap_start = curr_node->next;
             
             // since we're getting rid of this entry, we should reset and free the associated lock, too
-            del_lockaddr(llist_start, curr_node->metadata->lock_addr);
+            //      NOTE: possible to do this w/o having the entire dedicated llist structure
+            // del_lockaddr(llist_start, curr_node->metadata->lock_addr);
+            *(curr_node->metadata->lock_addr) = INVALID_KEY;
+            free((void*)(curr_node->metadata->lock_addr));
 
             // free the metadata struct
             free((void*)(curr_node->metadata));
@@ -142,6 +194,33 @@ void lmap_del(
 
     if (!found)
         ERR("No match found for %p when trying to delete an lmap_node\n", (void*)addr);
+}
+
+
+// NOTE: access_sz is the size of the actual access that's happening
+void chk_ptr_access(
+    lmap_node* lmap_start,
+    uintptr_t addr,
+    size_t access_sz
+) {
+    lmap_node* found_node = lmap_find(lmap_start, addr);
+    if (!found_node) {
+        // ptr doesn't exist - bad instrumentation, or intentional corruption
+        ERR("Pointer %p wasn't found in lmap!\n", (void*)addr);
+    }
+
+    // Shyamoli
+    if ((addr < found_node->metadata->base_addr) || (addr + (uintptr_t)access_sz > found_node->metadata->bound_addr)) {
+        // can change this handler easily
+        ERR("Spatial memory violation detected by Shyamoli: %p is out of the bounds %p to %p!\n",
+            (void*)addr, (void*)(found_node->metadata->base_addr), (void*)(found_node->metadata->bound_addr));
+    }
+
+    // Chhayanaut
+    if (found_node->metadata->key != *(found_node->metadata->lock_addr)) {
+        ERR("Temporal memory violation detected by Chhayanaut: %p's key (%llu) and lock (%llu) don't match!\n",
+            (void*)addr, found_node->metadata->key, *(found_node->metadata->lock_addr));
+    }
 }
 
 
--- test/test1.c
+++ test/test1.c
@@ -1,11 +1,9 @@
 #include "main.h"
-#include "llist.c"
 #include "lmap.c"
 
 
 // GLOBALS
 lmap_node* lmap_start = (lmap_node*)NULL;
-addr_node* llist_start = (addr_node*)NULL;
 
 typedef struct {
     int* y;
@@ -31,49 +29,84 @@ typedef struct {
 
 void print_vars() {
     lmap_print(lmap_start);
-    llist_print(llist_start);
-    DBG("Key counter: %llu\nLMap start: %p, LList start: %p\n\n", key_cnt, (void*)lmap_start, (void*)llist_start);
+    DBG("Key counter: %llu\nLMap start: %p\n\n", key_cnt, (void*)lmap_start);
 }
 
 
 int add_to_lists(void) {
     print_vars();
+    // 1. ALLOCATION
     // for each pointer allocation, lmap_add with the required info
     // happens after malloc for both SoftBound and CETS
     test_s* foo = MALLOC_T(test_s);
     // just 1 line of instrumentation!!
-    lmap_add(&lmap_start, &llist_start, (uintptr_t)foo, (uintptr_t)foo, (uintptr_t)foo + sizeof(test_s), FREEABLE);
-    print_vars();
+    lmap_add(&lmap_start, (uintptr_t)foo, (uintptr_t)foo, (uintptr_t)foo + sizeof(test_s), FREEABLE);
+    // print_vars();
 
+    // accessing foo->y is okay for now, since it doesn't threaten to go outside struct bounds?
+    //      once we see any pointer assignment, that's when we add another entry to lmap
     foo->y = MALLOC_T(int);
-    lmap_add(&lmap_start, &llist_start, (uintptr_t)(foo->y), (uintptr_t)(foo->y), (uintptr_t)(foo->y) + sizeof(int), FREEABLE);
-    print_vars();
+    lmap_add(&lmap_start, (uintptr_t)(foo->y), (uintptr_t)(foo->y), (uintptr_t)(foo->y) + sizeof(int), FREEABLE);
+    // print_vars();
 
+    // arrays my be{love,hate}d
+    // we won't consider dynamically allocated strings here, since they're a different beast in terms of memory access
+    const char pld[] = "12345";
+    lmap_add(&lmap_start, (uintptr_t)(pld), (uintptr_t)(pld), (uintptr_t)(pld) + sizeof(pld), FREEABLE);
+
+    // 2. POINTER ARITHMETIC
     // pointer copying/arithmetic propagates the metadata
     //      while unmodified pointer copying doesn't necessitate duplication of lmap entries,
     //      in my case, it makes it a lot more consistent and easier to script into C if I treat them the same
     int* copy = foo->y;
-    lmap_add(&lmap_start, &llist_start, (uintptr_t)copy, (uintptr_t)(foo->y), (uintptr_t)(foo->y) + sizeof(int), NON_FREEABLE);
+    lmap_add(&lmap_start, (uintptr_t)copy, (uintptr_t)(foo->y), (uintptr_t)(foo->y) + sizeof(int), NON_FREEABLE);
     // we only have to free ptrs that were directly returned by malloc!
 
     // pointer arithmetic!
     int* new = foo->y + 1;
     // the addr (key for lmap) is ALWAYS the new pointer's value, but the base is the pointer that this is based off
     //      NOTE: POSSIBLE ISSUE! can't use a pointer within a struct to access other elts of the struct. maybe okay?
-    lmap_add(&lmap_start, &llist_start, (uintptr_t)new, (uintptr_t)(foo->y), (uintptr_t)(foo->y) + sizeof(int), NON_FREEABLE);
+    lmap_add(&lmap_start, (uintptr_t)new, (uintptr_t)(foo->y), (uintptr_t)(foo->y) + sizeof(int), NON_FREEABLE);
+    // Q. if pointer arithmetic adds a legal entry to lmap, what stops an attacker from creating an out-of-bounds entry?
+    // A. the lmap entry inherits all base/bound info from the original ptr; checking happens on access!
+
+    // 3. POINTER ACCESS
+    // look up the ptr, perform the SoftBound AND CETS check
+    //      NOTE: addr should be the pointer getting dereferenced! but sizeof should check the underlying type
+    // enable this to see a temporal error (dangling ptr)
+    // lmap_del(&lmap_start, (uintptr_t)(foo->y));
+    chk_ptr_access(lmap_start, (uintptr_t)(copy), sizeof(*(copy)));
+    int y = *(copy);
+    // enable for a spatial "error" (going out of bounds)
+    // chk_ptr_access(lmap_start, (uintptr_t)(new), sizeof(*(new)));
+    // int z = *(new);
+    // (void) z;
+    chk_ptr_access(lmap_start, (uintptr_t)(foo), sizeof(*foo));
+    test_s bar = *foo;
+    // unrelated: arrays/buffers have different behaviour under sizeof (total data length), even though they're still ptrs
+    // the below doesn't work, because &pld[2] would need to be its own entry in lmap first through instrumentation
+    // chk_ptr_access(lmap_start, (uintptr_t)(&pld[2]), sizeof(pld[2]));
+    char* pld_elt = (char*)pld + 2;
+    lmap_add(&lmap_start, (uintptr_t)pld_elt, (uintptr_t)(pld), (uintptr_t)(pld) + sizeof(char), NON_FREEABLE);
+    chk_ptr_access(lmap_start, (uintptr_t)(pld_elt), sizeof(*pld_elt));
+    char c = pld[2];
+    (void) bar;
+    (void) y;
+    (void) c;
 
     // now, frees and deletes go hand in hand for each malloc
     // deleting two entries of the same ptr address... but is okay, because duplicates are allowed in lmap
     // NOTE: the program needs to ensure that everything has been freed
-    lmap_del(&lmap_start, &llist_start, (uintptr_t)new);
-    lmap_del(&lmap_start, &llist_start, (uintptr_t)copy);       // FIX WITH FIND~!
-    lmap_del(&lmap_start, &llist_start, (uintptr_t)(foo->y));
-    free(foo->y);
+    lmap_del(&lmap_start, (uintptr_t)new);
+    // ERR("a\n\n\n");
+    lmap_del(&lmap_start, (uintptr_t)copy);
+    lmap_del(&lmap_start, (uintptr_t)(foo->y));
+    // free(foo->y);
     print_vars();
     // NOTE: to free a struct, NEED TO FREE STRUCT MEMBERS BEFORE STRUCT ITSELF!!!
 
-    lmap_del(&lmap_start, &llist_start, (uintptr_t)foo);
-    free(foo);
+    lmap_del(&lmap_start, (uintptr_t)foo);
+    // free(foo);
     print_vars();
 
     // DBG("Lockaddr key:%llu\n\n\n", *lock_persist);
